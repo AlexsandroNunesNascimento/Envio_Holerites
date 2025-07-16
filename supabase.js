75fbb23286f124bd02ee0025fde78606d13836ba
Alex <alex@gmail.com>
2025-07-16 17:08:45 -0300

    CODIGO FINALIZADO RETIRADO OS COMENTARIOS E COLOCADO UM PASSO A PASSO PARA O BANCO DE DADOS

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = //cole aqui sua URL
-const supabaseKey = //cole aqui sua chave anom 
+const supabaseUrl = 'https://ikpnwedylgjvntovqrae.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrcG53ZWR5bGdqdm50b3ZxcmFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2ODE4NjUsImV4cCI6MjA2ODI1Nzg2NX0.QcQB_zSTEf5Xz719IlMM7c6uPwiWGrjFLRAd0PO0ALY';
 export const supabase = createClient(supabaseUrl, supabaseKey);
