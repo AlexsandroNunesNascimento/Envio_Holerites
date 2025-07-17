cf78a2f2baf6abd2054ac6781f08d9f38e5a6dda
Alex <alex@gmail.com>
2025-07-17 15:38:21 -0300

    Acrecentado o sistema de limpeza de banco de dados e pagar os holerites de 6 meses para traz

--- supabase.js
+++ supabase.js
@@ -4,11 +4,3 @@ const supabaseUrl = 'SUA_URL_AQUI';
 const supabaseKey = 'SUA_CHAVE_ANOM_AQUI';
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
-/*
-const supabaseUrl = 'https://ikpnwedylgjvntovqrae.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrcG53ZWR5bGdqdm50b3ZxcmFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2ODE4NjUsImV4cCI6MjA2ODI1Nzg2NX0.QcQB_zSTEf5Xz719IlMM7c6uPwiWGrjFLRAd0PO0ALY';
-
-const supabaseUrl = 'SUA_URL_AQUI';
-const supabaseKey = 'SUA_CHAVE_ANOM_AQUI';
-
-*/
